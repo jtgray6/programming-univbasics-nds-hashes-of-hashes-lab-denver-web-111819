a03c5fb084119dcee5c6f810902ae402543e5785
Jeremy Gray <jeremygray@utexas.edu>
2019-11-14 21:20:51 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -49,6 +49,12 @@ def naming_system
       label: "Class",
       sub_category: {
         label: "Order"
+        sub_category: {
+          label:"Family"
+          sub_category: {
+            label: "Order"
+          }
+        }
       }
     }
   }
